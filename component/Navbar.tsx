d0a61912d494f65db2b47429ff20893a4946c3b7
saix511 <saadirfan41@gmail.com>
2021-12-16 14:28:58 +0500

    f

--- component/Navbar.tsx
+++ component/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   if (loading)
     return (
       <div className=' min-h-screen flex justify-center items-center'>
-        <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-900'></div>
+        <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500'></div>
       </div>
     )
 
